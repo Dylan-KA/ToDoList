7b650644f4343c6fb0bcbefdbf0b806046e7646b
Dylan Archer <111679436+Dylan-KA@users.noreply.github.com>
2025-04-16 15:37:35 +1000

    Add project files.

---
+++ MauiApp1/ViewModel/MainViewModel.cs
@@ -0,0 +1,35 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+
+namespace MauiApp1.ViewModel
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        public MainViewModel()
+        {
+            Items = new ObservableCollection<string>();
+        }
+        
+        [ObservableProperty]
+        ObservableCollection<string> items;
+        
+        [ObservableProperty]
+        string text;
+
+        [RelayCommand]
+        void Add()
+        {
+            if(string.IsNullOrWhiteSpace(Text)) { return; }
+            Items.Add(text);
+            Text = String.Empty;
+        }
+
+        [RelayCommand]
+        void Delete(string toDelete)
+        {
+            if (Items.Contains(toDelete)) { Items.Remove(toDelete); }
+        }
+
+    }
+}
